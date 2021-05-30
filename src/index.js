900f0af1db085d3b810ab65915418963de6b0978
Ehren <ehren.chen.ej@gmail.com>
2021-05-31 01:15:09 +0800

    fix spelling

--- src/index.js
+++ src/index.js
@@ -24,4 +24,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-ServiceWorker.register();
+serviceWorker.register();
